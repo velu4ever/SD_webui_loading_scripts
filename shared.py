6f7915558336a61920c46b8dd6b7022f338d5fcf
velu4ever <56683234+velu4ever@users.noreply.github.com>
2023-09-14 06:38:04 +0530

    timezone update

--- shared.py
+++ shared.py
@@ -242,7 +242,7 @@ options_templates = {}
 options_templates.update(options_section(('saving-images', "Saving images/grids"), {
     "samples_save": OptionInfo(True, "Always save all generated images"),
     "samples_format": OptionInfo('png', 'File format for images'),
-    "samples_filename_pattern": OptionInfo("[datetime<%Y%m%d_%H%M%S>]-[seed]-[model_name]", "Images filename pattern", component_args=hide_dirs),
+    "samples_filename_pattern": OptionInfo("[datetime<%Y%m%d_%H%M%S><Asia/Kolkata>]-[seed]-[model_name]", "Images filename pattern", component_args=hide_dirs),
     "save_images_add_number": OptionInfo(True, "Add number to filename when saving", component_args=hide_dirs),
 
     "grid_save": OptionInfo(False, "Always save all generated image grids"),
@@ -306,7 +306,7 @@ options_templates.update(options_section(('saving-to-dirs', "Saving to a directo
     "save_to_dirs": OptionInfo(True, "Save images to a subdirectory"),
     "grid_save_to_dirs": OptionInfo(True, "Save grids to a subdirectory"),
     "use_save_to_dirs_for_ui": OptionInfo(False, "When using \"Save\" button, save images to a subdirectory"),
-    "directories_filename_pattern": OptionInfo("[date]", "Directory name pattern", component_args=hide_dirs),
+    "directories_filename_pattern": OptionInfo("[datetime<%Y-%m-%d><Asia/Kolkata>]", "Directory name pattern", component_args=hide_dirs),
     "directories_max_prompt_words": OptionInfo(8, "Max prompt words for [prompt_words] pattern", gr.Slider, {"minimum": 1, "maximum": 20, "step": 1, **hide_dirs}),
 }))
 
